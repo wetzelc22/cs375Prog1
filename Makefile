1bc9113e3e5939a05f2daf9f5633e578de1e3d4c
unknown <cwetzel2@binghamton.edu>
2020-03-16 20:56:53 -0400

    program2 written need to compile and debug, makefile updated to run prog2

--- Makefile
+++ Makefile
@@ -1,13 +1,19 @@
 
-all: program1
+all: program1 program2
 
 program1.o: program1.cpp
 	g++ -c program1.cpp -o program1.o
 program1: program1.o
 	g++ program1.o -o program1
+program2.o: program2.cpp
+	g++ -c program2.cpp -o program2.o
+program2: program2.o
+	g++ program2.o -o program2
 
 run1: program1
 	./program1 filex.txt filey.txt output1.txt
+run2: program2
+	./program2 filex.txt filey.txt output2.txt
 
 clean: 
 	rm *.o program1 program2 program3
--- program2.cpp
+++ program2.cpp
@@ -1,12 +1,27 @@
+#include <iostream>
+#include <stdio.h>
+#include <vector>
+#include <fstream>
+#include <string>
+#include <chrono>
+#include <cassert>
+#include <algorithm>
 
 
-
-
-
+int longestCS(std::string s1, std::string s2){
+	if(s1.empty() || s2.empty()){
+		return 0;
+	}
+	if(s1.back() == s2.back()){
+		return 1 + longestCS(s1.substr(0,s1.size()-1), s2.substr(0,s2.size()-1));
+	}else{
+		return max(longestCS(s1.substr(0,s1.size()), s2.substr(0,s2.size()-1)), longestCSs1.substr(0,s1.size()-1), s2.substr(0,s2.size()))
+	}
+}
 
 
 int main(int argc, char **argv){
-	std::chrono start = high_resolution_clock::now();
+	auto start = std::chrono::high_resolution_clock::now();
 	assert(argc == 4);
 	std::string x;
 	std::string y;
@@ -16,23 +31,12 @@ int main(int argc, char **argv){
 	reader2 >> y;
 	reader.close(); reader2.close();
 
-	int n = s1.size();
-	int m = s2.size();
-	vector<vector<int> > mat(n+1, vector<int>(m+1, 0));
-	int len = topDown(mat, s1, s2);
+	int len = longestCS(x, y);
 
-
-	std::chrono end = high_resolution_clock::now();
-	std::chrono time = duration_cast<seconds>(end-start);
+	auto end = std::chrono::high_resolution_clock::now();
+	std::chrono::duration<double> time = end-start;
+	
 	std::ofstream output(argv[3]);
-	if(n < 10 || m < 10){
-		for(int i = 0; i < n+1; i++){
-			for(int j = 0; j < m+1; j++){
-				output << mat[i][j] << " " ;
-			}
-			output << "\n";
-		}
-	}
 	output << len << "\n";
 	output << time.count() << " seconds\n";
 	output.close();
